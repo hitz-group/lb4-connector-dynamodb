7687616261999da78c6af04fce19fde1a7cbfee6
varun <varun@tillpos.com.au>
2019-05-26 01:32:54 +0530

    Fixed the TestCases and some missed callbacks
    - Formatted the code and added vscode workspace settings

--- test/basic-querying.test.js
+++ test/basic-querying.test.js
@@ -1,7 +1,7 @@
 const should = require('./init.js');
 
-describe('basic-querying', function() {
-  before(function(done) {
+describe('basic-querying', function () {
+  before(function (done) {
     db = getSchema();
 
     User = db.define('User', {
@@ -29,7 +29,7 @@ describe('basic-querying', function() {
     });
 
     let modelCreated = 0;
-    db.adapter.emitter.on('created', function() {
+    db.adapter.emitter.on('created', function () {
       modelCreated++;
       if (modelCreated === 4) {
         done();
@@ -37,11 +37,11 @@ describe('basic-querying', function() {
     });
   });
 
-  after(function(done) {
-    db.adapter.client.deleteTable({ TableName: 'Film' }, function() {
-      db.adapter.client.deleteTable({ TableName: 'User' }, function() {
-        db.adapter.client.deleteTable({ TableName: 'Book' }, function() {
-          db.adapter.client.deleteTable({ TableName: 'Song' }, function() {
+  after(function (done) {
+    db.adapter.client.deleteTable({ TableName: 'Film' }, function () {
+      db.adapter.client.deleteTable({ TableName: 'User' }, function () {
+        db.adapter.client.deleteTable({ TableName: 'Book' }, function () {
+          db.adapter.client.deleteTable({ TableName: 'Song' }, function () {
             done();
           });
         });
@@ -49,9 +49,9 @@ describe('basic-querying', function() {
     });
   });
 
-  describe('create items', function() {
-    it('should create a new item', function(done) {
-      User.create({ id: '1', name: 'Jonh', email: 'jonh@doe.foo' }, function(err, user) {
+  describe('create items', function () {
+    it('should create a new item', function (done) {
+      User.create({ id: '1', name: 'Jonh', email: 'jonh@doe.foo' }, function (err, user) {
         should.not.exist(err);
         should.exist(user);
         user.should.have.property('id', '1');
@@ -60,8 +60,8 @@ describe('basic-querying', function() {
       });
     });
 
-    it('should assign a hash key if not specified', function(done) {
-      Film.create({ title: 'Star Wars', year: 1977 }, function(err, cookie) {
+    it('should assign a hash key if not specified', function (done) {
+      Film.create({ title: 'Star Wars', year: 1977 }, function (err, cookie) {
         should.not.exist(err);
         cookie.should.have.property('id');
         db.adapter._models['Film'].hashKey.should.eql('id');
@@ -70,8 +70,8 @@ describe('basic-querying', function() {
       });
     });
 
-    it('should assign same value as hash key to id attribute', function(done) {
-      Book.create({ title: 'The Lord of the Rings', iban: '123456' }, function(err, book) {
+    it('should assign same value as hash key to id attribute', function (done) {
+      Book.create({ title: 'The Lord of the Rings', iban: '123456' }, function (err, book) {
         should.not.exist(err);
         should.exist(book);
         book.should.have.property('id', '123456');
@@ -79,13 +79,8 @@ describe('basic-querying', function() {
       });
     });
 
-    it('should replace original record if same hash key is provided', function(done) {
-      const tempUser = new User({
-        id: '1',
-        name: 'Johnny Doey',
-        email: 'johnny@doey.com'
-      });
-      User.create(tempUser, function(err, user) {
+    it('should replace original record if same hash key is provided', function (done) {
+      User.create({ id: '1', name: 'Johnny Doey', email: 'johnny@doey.com' }, function (err, user) {
         should.not.exist(err);
         user.should.have.property('id', '1');
         user.should.have.property('name', 'Johnny Doey');
@@ -94,13 +89,8 @@ describe('basic-querying', function() {
       });
     });
 
-    it('should handle undefined and null attributes and return the same from database', function(done) {
-      const tempUser = new User({
-        id: '2',
-        name: 'Anne',
-        email: null
-      });
-      User.create(tempUser, function(err, user) {
+    it('should handle undefined and null attributes and return the same from database', function (done) {
+      User.create({ id: '2', name: 'Anne', email: null }, function (err, user) {
         should.not.exist(err);
         (user.foo === undefined).should.be.true;
         (user.name === 'Anne').should.be.true;
@@ -109,29 +99,26 @@ describe('basic-querying', function() {
       });
     });
 
-    it('should return error saying hash key cannot be null', function(done) {
-      const tempUser = new User({
-        id: null
-      });
-      User.create(tempUser, function(err, user) {
+    it('should return error saying hash key cannot be null', function (done) {
+      User.create({ id: null }, function (err, user) {
         should.exist(err);
         done();
       });
     });
   });
 
-  describe('find', function() {
-    it('should query by id: not found', function(done) {
-      User.find('foo_id', function(err, user) {
+  describe('find', function () {
+    it('should query by id: not found', function (done) {
+      User.find('foo_id', function (err, user) {
         should.not.exist(user);
         should.not.exist(err);
         done();
       });
     });
 
-    it('should query by id: found', function(done) {
-      User.create({ id: '123' }, function(err, user) {
-        User.find(user.id, function(err, user) {
+    it('should query by id: found', function (done) {
+      User.create({ id: '123' }, function (err, user) {
+        User.find(user.id, function (err, user) {
           should.exist(user);
           should.not.exist(err);
           user.should.be.an.instanceOf(User);
@@ -141,10 +128,10 @@ describe('basic-querying', function() {
     });
   });
 
-  describe('all', function() {
+  describe('all', function () {
     before(seed);
-    it('should query collection', function(done) {
-      User.all(function(err, users) {
+    it('should query collection', function (done) {
+      User.all(function (err, users) {
         should.exists(users);
         should.not.exists(err);
         users.should.have.lengthOf(6);
@@ -152,8 +139,8 @@ describe('basic-querying', function() {
       });
     });
 
-    it('should query limited collection', function(done) {
-      User.all({ limit: 3 }, function(err, users) {
+    it('should query limited collection', function (done) {
+      User.all({ limit: 3 }, function (err, users) {
         should.exists(users);
         should.not.exists(err);
         users.should.have.lengthOf(3);
@@ -161,8 +148,8 @@ describe('basic-querying', function() {
       });
     });
 
-    it('should query offset collection with limit', function(done) {
-      User.all({ skip: 1, limit: 4 }, function(err, users) {
+    it('should query offset collection with limit', function (done) {
+      User.all({ skip: 1, limit: 4 }, function (err, users) {
         users[0].should.have.property('id', '2');
         should.exists(users);
         should.not.exists(err);
@@ -171,8 +158,8 @@ describe('basic-querying', function() {
       });
     });
 
-    it('should query filtered collection', function(done) {
-      User.all({ where: { role: 'lead' } }, function(err, users) {
+    it('should query filtered collection', function (done) {
+      User.all({ where: { role: 'lead' } }, function (err, users) {
         should.exists(users);
         should.not.exists(err);
         users.should.have.lengthOf(2);
@@ -180,30 +167,30 @@ describe('basic-querying', function() {
       });
     });
 
-    it('should query collection sorted by numeric field', function(done) {
-      User.all({ order: 'order' }, function(err, users) {
+    it('should query collection sorted by numeric field', function (done) {
+      User.all({ order: 'order' }, function (err, users) {
         should.exists(users);
         should.not.exists(err);
-        users.forEach(function(u, i) {
+        users.forEach(function (u, i) {
           u.order.should.eql(i + 1);
         });
         done();
       });
     });
 
-    it('should query collection desc sorted by numeric field', function(done) {
-      User.all({ order: 'order DESC' }, function(err, users) {
+    it('should query collection desc sorted by numeric field', function (done) {
+      User.all({ order: 'order DESC' }, function (err, users) {
         should.exists(users);
         should.not.exists(err);
-        users.forEach(function(u, i) {
+        users.forEach(function (u, i) {
           u.order.should.eql(users.length - i);
         });
         done();
       });
     });
 
-    it('should query collection sorted by string field', function(done) {
-      User.all({ order: 'name' }, function(err, users) {
+    it('should query collection sorted by string field', function (done) {
+      User.all({ order: 'name' }, function (err, users) {
         should.exists(users);
         should.not.exists(err);
         users.shift().name.should.equal('George Harrison');
@@ -213,8 +200,8 @@ describe('basic-querying', function() {
       });
     });
 
-    it('should query collection desc sorted by string field', function(done) {
-      User.all({ order: 'name DESC' }, function(err, users) {
+    it('should query collection desc sorted by string field', function (done) {
+      User.all({ order: 'name DESC' }, function (err, users) {
         should.exists(users);
         should.not.exists(err);
         users.pop().name.should.equal('George Harrison');
@@ -225,11 +212,11 @@ describe('basic-querying', function() {
     });
   });
 
-  describe('count', function() {
+  describe('count', function () {
     before(seed);
 
-    it('should query total count', function(done) {
-      User.count(function(err, n) {
+    it('should query total count', function (done) {
+      User.count(function (err, n) {
         should.not.exist(err);
         should.exist(n);
         n.should.equal(6);
@@ -237,8 +224,8 @@ describe('basic-querying', function() {
       });
     });
 
-    it('should query filtered count', function(done) {
-      User.count({ role: 'lead' }, function(err, n) {
+    it('should query filtered count', function (done) {
+      User.count({ role: 'lead' }, function (err, n) {
         should.not.exist(err);
         should.exist(n);
         n.should.equal(2);
@@ -247,12 +234,12 @@ describe('basic-querying', function() {
     });
   });
 
-  describe('findOne', function() {
+  describe('findOne', function () {
     before(seed);
 
-    it('should work even when find by id', function(done) {
-      User.findOne(function(e, u) {
-        User.findOne({ where: { id: u.id } }, function(err, user) {
+    it('should work even when findOne by id', function (done) {
+      User.findOne(function (e, u) {
+        User.findOne({ where: { id: u.id } }, function (err, user) {
           should.not.exist(err);
           should.exist(user);
           done();
@@ -261,12 +248,12 @@ describe('basic-querying', function() {
     });
   });
 
-  describe('exists', function() {
+  describe('exists', function () {
     before(seed);
 
-    it('should check whether record exist', function(done) {
-      User.findOne(function(e, u) {
-        User.exists(u.id, function(err, exists) {
+    it('should check whether record exist', function (done) {
+      User.findOne(function (e, u) {
+        User.exists(u.id, function (err, exists) {
           should.not.exist(err);
           should.exist(exists);
           exists.should.be.ok;
@@ -275,9 +262,9 @@ describe('basic-querying', function() {
       });
     });
 
-    it('should check whether record not exist', function(done) {
-      User.destroyAll(function() {
-        User.exists('asdasd', function(err, exists) {
+    it('should check whether record not exist', function (done) {
+      User.destroyAll(function () {
+        User.exists('asdasd', function (err, exists) {
           should.not.exist(err);
           exists.should.not.be.ok;
           done();
@@ -310,8 +297,8 @@ function seed(done) {
     { id: '6', name: 'Stuart Sutcliffe', order: 3 }
   ];
 
-  User.destroyAll(function() {
-    beatles.forEach(function(beatle) {
+  User.destroyAll(function () {
+    beatles.forEach(function (beatle) {
       User.create(beatle, ok);
     });
   });
